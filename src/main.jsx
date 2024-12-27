a1606162025a04b3f4367af284ad6a05a28fce25
DasunNethsara-04 <techsaralk.pro@gmail.com>
2024-12-27 23:15:25 +0530

    route added for dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 import App from "./App.jsx";
 import Login from "./Pages/Auth/Login.jsx";
 import Register from "./Pages/Auth/Register.jsx";
+import Dashboard from "./Pages/Dashboard/Dashboard.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
@@ -11,6 +12,7 @@ createRoot(document.getElementById("root")).render(
       <Route path="/" element={<App />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   </BrowserRouter>
 );
