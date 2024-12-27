d13f0ed1ec09edb24c021859163eb79e0e901861
DasunNethsara-04 <techsaralk.pro@gmail.com>
2024-12-27 23:15:09 +0530

    dummy dashboard page added

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Dashboard = () => {
     return (
         <>
-            <h1>Dashboard</h1>
+            <h1>Welcome to Pomodoro Planner Dashboard</h1>
         </>
     )
 }
