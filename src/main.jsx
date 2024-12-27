5a9714ba3f951b1372a50748b08147669fbd373b
DasunNethsara-04 <techsaralk.pro@gmail.com>
2024-12-27 14:23:01 +0530

    public routes added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import App from "./App.jsx";
+import Login from "./Pages/Auth/Login.jsx";
+import Register from "./Pages/Auth/Register.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
   </BrowserRouter>
 );
