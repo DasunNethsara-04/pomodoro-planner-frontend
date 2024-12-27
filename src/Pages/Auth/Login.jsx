9907e59f9ba3df36ef244d2a74061a7adb29368a
DasunNethsara-04 <techsaralk.pro@gmail.com>
2024-12-27 14:29:44 +0530

    dependency error fixed in Login page

--- src/Pages/Auth/Login.jsx
+++ src/Pages/Auth/Login.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import NavBar from '../../Components/NavBar'
+import { Card, Col, Container, Row } from 'react-bootstrap'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
@@ -12,20 +16,14 @@ const Login = () => {
             <Card.Header><h1 className='text-center'>Login</h1></Card.Header>
             <Card.Body>
               <Form>
-                <Row>
-                  <Col md="6">
-                    <Form.Group id='email' className='mb-3'>
-                      <Form.Label>Email</Form.Label>
-                      <Form.Control type='email' name='email' required />
-                    </Form.Group>
-                  </Col>
-                  <Col md="3">
-                    <Form.Group id='password' className='mb-3'>
-                      <Form.Label>Password</Form.Label>
-                      <Form.Control type='password' name='password' required />
-                    </Form.Group>
-                  </Col>
-                </Row>
+                <Form.Group id='email' className='mb-3'>
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control type='email' name='email' required />
+                </Form.Group>
+                <Form.Group id='password' className='mb-3'>
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type='password' name='password' required />
+                </Form.Group>
                 <Button type='submit' className='w-100'>Login</Button>
               </Form>
               <div className='w-100 text-center mt-2'>
