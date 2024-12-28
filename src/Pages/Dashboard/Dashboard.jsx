e5af0e05eb99f7824996d17c1b49e885a75dce1e
DasunNethsara-04 <techsaralk.pro@gmail.com>
2024-12-28 17:25:54 +0530

    refactor Dashboard component to use imported verifyToken utility function for authentication

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,32 +1,15 @@
 import React, { useEffect } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
+import { verifyToken } from '../../util'
 
 const Dashboard = () => {
     const navigate = useNavigate();
     useEffect(() => {
-        const verifyToken = async () => {
-            const token = localStorage.getItem("token");
-            try {
-                const response = await axios.get(`http://127.0.0.1:8000/auth/verify-token/${token}`);
-                if (response.status !== 200) {
-                    throw new Error("Invalid Token");
-                }
-            } catch (error) {
-                localStorage.removeItem("token");
-                navigate("/login");
-                if (error.response) {
-                    console.error("Server Error: " + error.response.data);
-                } else if (error.request) {
-                    console.error("Request Error: " + error.request);
-                } else {
-                    console.error("Unexpected Error: " + error.message);
-                }
-            }
-        }
-
         verifyToken();
-    }, [navigate])
+    }, [navigate]);
+
+
     return (
         <>
             <h1>Welcome to Pomodoro Planner Dashboard</h1>
