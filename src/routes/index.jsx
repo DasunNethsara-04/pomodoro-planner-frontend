13b9b9fd94a450e4d840da44b6b4ecca7c7ed20d
DasunNethsara-04 <techsaralk.pro@gmail.com>
2024-12-29 15:00:58 +0530

    refactor routing setup for improved readability and consistency

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,4 +1,9 @@
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom"
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    RouterProvider,
+} from "react-router-dom";
 import Home from "../Pages/Home";
 import Login from "../Pages/Auth/Login";
 import Register from "../Pages/Auth/Register";
@@ -6,9 +11,9 @@ import Protected from "./protected";
 import Dashboard from "../Pages/Dashboard/Dashboard";
 import { isAuthenticated } from "./helpers";
 import Timer from "../Pages/Dashboard/Timer/Timer";
-import NewTodo from '../Pages/Dashboard/Todo/New';
-import ShowTodo from '../Pages/Dashboard/Todo/Show';
-import Statistics from '../Pages/Dashboard/Statistics/Statistics';
+import NewTodo from "../Pages/Dashboard/Todo/New";
+import ShowTodo from "../Pages/Dashboard/Todo/Show";
+import Statistics from "../Pages/Dashboard/Statistics/Statistics";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
@@ -23,16 +28,24 @@ const router = createBrowserRouter(
                 <Route path="/stat" element={<Statistics />} />
             </Route>
             <Route path="/" element={<Home />} />,
-            <Route path="/login" element={<Login />} loader={async () => await isAuthenticated()} />,
-            <Route path="/register" element={<Register />} loader={async () => await isAuthenticated()} />
+            <Route
+                path="/login"
+                element={<Login />}
+                loader={async () => await isAuthenticated()}
+            />
+            ,
+            <Route
+                path="/register"
+                element={<Register />}
+                loader={async () => await isAuthenticated()}
+            />
             <Route path="*" element={<Home />} />
         </Route>
     )
 );
 
 const Index = () => {
-    return <RouterProvider router={router} />
+    return <RouterProvider router={router} />;
 };
-
 
 export default Index;
