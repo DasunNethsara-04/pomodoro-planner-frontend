c7c09b1a711876fdcd290aaaa55eaa20a5d09a69
DasunNethsara-04 <techsaralk.pro@gmail.com>
2024-12-29 14:57:17 +0530

    add routes for Timer, NewTodo, ShowTodo, and Statistics components in Dashboard

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,12 +5,22 @@ import Register from "../Pages/Auth/Register";
 import Protected from "./protected";
 import Dashboard from "../Pages/Dashboard/Dashboard";
 import { isAuthenticated } from "./helpers";
+import Timer from "../Pages/Dashboard/Timer/Timer";
+import NewTodo from '../Pages/Dashboard/Todo/New';
+import ShowTodo from '../Pages/Dashboard/Todo/Show';
+import Statistics from '../Pages/Dashboard/Statistics/Statistics';
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/">
             <Route element={<Protected />}>
                 <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/timer" element={<Timer />} />
+                <Route path="/todo">
+                    <Route path="/todo/new" element={<NewTodo />} />
+                    <Route path="/todo/show" element={<ShowTodo />} />
+                </Route>
+                <Route path="/stat" element={<Statistics />} />
             </Route>
             <Route path="/" element={<Home />} />,
             <Route path="/login" element={<Login />} loader={async () => await isAuthenticated()} />,
