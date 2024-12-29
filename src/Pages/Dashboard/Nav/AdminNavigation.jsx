237150b84bf3b655154089b3c68233ab2a30ff2a
DasunNethsara-04 <techsaralk.pro@gmail.com>
2024-12-29 14:57:26 +0530

    refactor AdminNavigation component to use React Router links for improved navigation

--- src/Pages/Dashboard/Nav/AdminNavigation.jsx
+++ src/Pages/Dashboard/Nav/AdminNavigation.jsx
@@ -2,23 +2,26 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
 
 function AdminNavigation() {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container fluid>
-                <Navbar.Brand href="#home">Pomodoro Planner</Navbar.Brand>
+                <Navbar.Brand as={Link} to="/dashboard">Pomodoro Planner</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#link">Timer</Nav.Link>
+                        <Nav.Link as={Link} to="/timer">Timer</Nav.Link>
                         <NavDropdown title="Todo" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.2">
+                            <NavDropdown.Item as={Link} to="/todo/new">
                                 New Todo
                             </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Show Todos</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="/todo/show">
+                                Show Todos
+                            </NavDropdown.Item>
                         </NavDropdown>
-                        <Nav.Link href="#link">Statistics</Nav.Link>
+                        <Nav.Link href="/stat">Statistics</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
