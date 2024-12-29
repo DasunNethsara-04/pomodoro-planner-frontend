7d2709da7f5fa43ed7382c5118a80d5dceabe39e
DasunNethsara-04 <techsaralk.pro@gmail.com>
2024-12-29 14:26:03 +0530

    refactor Dashboard component to include AdminNavigation and remove token verification logic

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,18 +1,9 @@
-import React, { useEffect } from 'react'
-import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
-import { verifyToken } from '../../util'
+import AdminNavigation from './Nav/AdminNavigation';
 
 const Dashboard = () => {
-    const navigate = useNavigate();
-    useEffect(() => {
-        verifyToken();
-    }, [navigate]);
-
-
     return (
         <>
-            <h1>Welcome to Pomodoro Planner Dashboard</h1>
+            <AdminNavigation />
         </>
     )
 }
