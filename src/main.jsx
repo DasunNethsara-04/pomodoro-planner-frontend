54aa7dafc427c60af7c2954d929d327e25fa4b83
DasunNethsara-04 <techsaralk.pro@gmail.com>
2024-12-29 14:14:28 +0530

    remove routing setup from main entry point and render App directly

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,7 @@
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 import App from "./App.jsx";
-import Login from "./Pages/Auth/Login.jsx";
-import Register from "./Pages/Auth/Register.jsx";
-import Dashboard from "./Pages/Dashboard/Dashboard.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-    </Routes>
-  </BrowserRouter>
+  <App />
 );
