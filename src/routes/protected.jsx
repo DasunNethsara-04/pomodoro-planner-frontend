e5ac6848caeead991886840e798c42d7c040b8f7
DasunNethsara-04 <techsaralk.pro@gmail.com>
2024-12-29 14:26:12 +0530

    implement token verification in Protected route component for enhanced security

--- src/routes/protected.jsx
+++ src/routes/protected.jsx
@@ -1,8 +1,33 @@
 import { Navigate, Outlet } from "react-router-dom";
+import axios from 'axios'
+import { useEffect, useState } from "react";
+
+const verifyToken = async () => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+        return false;
+    }
+    try {
+        const response = await axios.get(`http://127.0.0.1:8000/auth/verify-token/${token}`);
+        return response.status === 200;
+    } catch (error) {
+        localStorage.removeItem("token");
+        return false;
+    }
+}
 
 const Protected = () => {
-    const token = localStorage.getItem("token");
-    return token ? <Outlet /> : <Navigate to="/login" />;
+    const [isAuthorized, setIsAuthorized] = useState(null);
+    useEffect(() => {
+        const checkAuth = async () => {
+            const result = await verifyToken();
+            setIsAuthorized(result)
+        };
+        checkAuth();
+    }, []);
+
+
+    return isAuthorized ? <Outlet /> : <Navigate to="/login" />;
 }
 
 
