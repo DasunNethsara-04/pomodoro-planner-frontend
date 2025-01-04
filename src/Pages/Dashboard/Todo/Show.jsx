eb31e0155a94e6325a69332383d690a50f455708
DasunNethsara-04 <techsaralk.pro@gmail.com>
2025-01-04 22:01:09 +0530

    fetch and display todos in Show component with API integration

--- src/Pages/Dashboard/Todo/Show.jsx
+++ src/Pages/Dashboard/Todo/Show.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AdminNavigation from '../Nav/AdminNavigation';
+import axios from 'axios';
 const ShowTodo = () => {
+
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        const fetchTodos = async () => {
+            // Fetch all todos
+            const response = await axios.get("http://127.0.0.1:8000/api/todos", {
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${localStorage.getItem('token')}`
+                }
+            });
+            if (response.status === 200) {
+                console.log(response.data.todos);
+                setTodos(response.data.todos);
+            } else {
+                console.error("An error occurred while fetching todos");
+            }
+        }
+        fetchTodos();
+    }, [])
     return (
         <>
             <AdminNavigation />
