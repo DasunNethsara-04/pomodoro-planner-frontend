5910c01d52c570709d6ec4f7426d6d511ae84f89
DasunNethsara-04 <techsaralk.pro@gmail.com>
2025-01-07 22:50:59 +0530

    feat: add edit functionality for todos with a modal form in Show component

--- src/Pages/Dashboard/Todo/Show.jsx
+++ src/Pages/Dashboard/Todo/Show.jsx
@@ -1,15 +1,19 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import AdminNavigation from '../Nav/AdminNavigation';
-import { Container, Table } from 'react-bootstrap';
-import { RiFileEditLine } from 'react-icons/ri';
-import { FaTrashCan } from 'react-icons/fa6';
-import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import AdminNavigation from "../Nav/AdminNavigation";
+import { Container, Form, Table } from "react-bootstrap";
+import { RiFileEditLine } from "react-icons/ri";
+import { FaTrashCan } from "react-icons/fa6";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 
 const ShowTodo = () => {
-
     const [todos, setTodos] = useState([]);
+
+    const [editTitle, setEditTitle] = useState("");
+    const [editDescription, setEditDescription] = useState("");
+    const [editDueDate, setEditDueDate] = useState("");
+    const [editTodoStatus, setEditTodoStatus] = useState("");
 
     const [show, setShow] = useState(false);
     const handleShow = () => setShow(true);
@@ -21,8 +25,8 @@ const ShowTodo = () => {
             const response = await axios.get("http://127.0.0.1:8000/api/todos", {
                 headers: {
                     "Content-Type": "application/json",
-                    "Authorization": `Bearer ${localStorage.getItem('token')}`
-                }
+                    Authorization: `Bearer ${localStorage.getItem("token")}`,
+                },
             });
             if (response.status === 200) {
                 console.log(response.data.todos);
@@ -30,14 +34,27 @@ const ShowTodo = () => {
             } else {
                 console.error("An error occurred while fetching todos");
             }
-        }
+        };
         fetchTodos();
-    }, [])
+    }, []);
+
+    const handleEditTodoForm = (e) => {
+        console.log("fvdfv");
+        e.preventDefault();
+
+        const formData = new FormData();
+        formData.append("title", editTitle);
+        formData.append("description", editDescription);
+        formData.append("dueDate", editDueDate);
+        formData.append("status", editTodoStatus);
+
+        console.log(formData);
+    };
 
     return (
         <>
             <AdminNavigation />
-            <Container className='mt-4'>
+            <Container className="mt-4">
                 <h1 className="display-6">Todos</h1>
                 <div className="table-responsive">
                     <Table striped bordered hover>
@@ -58,8 +75,15 @@ const ShowTodo = () => {
                                     <td>{todo.description}</td>
                                     <td>{todo.dueDate}</td>
                                     <td>
-                                        <button className="btn-sm btn btn-outline-warning" onClick={handleShow}><RiFileEditLine /></button>
-                                        <button className="ms-2 btn-sm btn btn-outline-danger"><FaTrashCan /></button>
+                                        <button
+                                            className="btn-sm btn btn-outline-warning"
+                                            onClick={handleShow}
+                                        >
+                                            <RiFileEditLine />
+                                        </button>
+                                        <button className="ms-2 btn-sm btn btn-outline-danger">
+                                            <FaTrashCan />
+                                        </button>
                                     </td>
                                 </tr>
                             ))}
@@ -68,26 +92,77 @@ const ShowTodo = () => {
                 </div>
             </Container>
 
-
-
-            <div className="model" style={{ display: 'block', position: 'initial' }} show={show} onHide={handleClose}>
-                <Modal show={show} onHide={handleClose}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>Edit Todo</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
-                    <Modal.Footer>
-                        <Button variant="secondary" onClick={handleClose}>
-                            Close
-                        </Button>
-                        <Button variant="primary" onClick={handleClose}>
-                            Save Changes
-                        </Button>
-                    </Modal.Footer>
-                </Modal>
+            <div className="model" style={{ display: "block", position: "initial" }}>
+                <div
+                    className="model"
+                    style={{ display: "block", position: "initial" }}
+                >
+                    <Modal show={show} onHide={handleClose}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Edit Todo</Modal.Title>
+                        </Modal.Header>
+                        <Form onSubmit={handleEditTodoForm}>
+                            <Modal.Body>
+                                <Form.Group controlId="editTitle" className="mb-3">
+                                    <Form.Label>Todo Title</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        name="title"
+                                        placeholder="Buy food"
+                                        onChange={(e) => setEditTitle(e.target.value)}
+                                        required
+                                    />
+                                </Form.Group>
+                                <Form.Group controlId="editDescription" className="mb-3">
+                                    <Form.Label>Todo Description</Form.Label>
+                                    <Form.Control
+                                        as="textarea"
+                                        name="description"
+                                        onChange={(e) => setEditDescription(e.target.value)}
+                                        placeholder="Buy some food from supermarket for dinner"
+                                    />
+                                </Form.Group>
+                                <Form.Group controlId="editDueDate" className="mb-3">
+                                    <Form.Label>Due Date</Form.Label>
+                                    <Form.Control
+                                        type="date"
+                                        name="dueDate"
+                                        onChange={(e) => setEditDueDate(e.target.value)}
+                                        required
+                                    />
+                                </Form.Group>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Done?</Form.Label>
+                                    <Form.Check
+                                        type="radio"
+                                        label="Yes"
+                                        name="done"
+                                        value="1"
+                                        onChange={(e) => setEditTodoStatus(e.target.value)}
+                                    />
+                                    <Form.Check
+                                        type="radio"
+                                        label="No"
+                                        name="done"
+                                        value="0"
+                                        onChange={(e) => setEditTodoStatus(e.target.value)}
+                                    />
+                                </Form.Group>
+                            </Modal.Body>
+                            <Modal.Footer>
+                                <Button variant="warning" type="submit">
+                                    Save
+                                </Button>
+                                <Button variant="secondary" onClick={handleClose}>
+                                    Close
+                                </Button>
+                            </Modal.Footer>
+                        </Form>
+                    </Modal>
+                </div>
             </div>
         </>
-    )
-}
+    );
+};
 
-export default ShowTodo
+export default ShowTodo;
