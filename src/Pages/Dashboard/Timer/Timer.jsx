6cf39621b852875ee0a8e47b66bbbb08e95b9826
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-02-23 11:40:51 +0530

    feat: integrate PomodoroTimer component into Timer page

--- src/Pages/Dashboard/Timer/Timer.jsx
+++ src/Pages/Dashboard/Timer/Timer.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import AdminNavigation from '../Nav/AdminNavigation'
+import PomodoroTimer from '../../../Components/PomodoroTimer/PomodoroTimer'
 
 const Timer = () => {
     return (
         <>
             <AdminNavigation />
-            Timer
+            <div className="App">
+                <header className="App-header">
+                    <PomodoroTimer />
+                </header>
+            </div>
         </>
     )
 }
